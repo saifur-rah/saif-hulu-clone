8eba90e4515b8ade644974090abe91d2f2dd67c2
unknown <saifurrahman8051@gmail.comgit config --global user.email saifurrahman8051@gmail.com>
2022-02-05 05:58:19 +0530

    second commit

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import {ThumbUpIcon} from "@heroicons/react/outline";
-import { forwardRef } from "react";
 
-const Thumbnail = forwardRef(({result} , ref)=> {
+
+function Thumbnail  ({result} ) {
 const BASE_URL = "https://image.tmdb.org/t/p/original/";
-return( <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+return( <div  className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
      <Image
         layout='responsive'
         src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`}
@@ -25,6 +25,6 @@ return( <div ref={ref} className="p-2 group cursor-pointer transition duration-2
 
   </div>
 );
-})
+}
 
 export default Thumbnail;
